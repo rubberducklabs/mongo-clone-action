5da1c21629df2aa5ecaabedfafd4b25def0c6daa
Somebody25 <marcel.koerbler@icloud.com>
2024-08-14 09:26:12 +0200

    Add exclude collection

--- clone.sh
+++ clone.sh
@@ -2,7 +2,17 @@
 
 MONGO_URI_SOURCE=$1
 MONGO_URI_TARGET=$2
+EXCLUDE_COLLECTION=$3
 DB_NAME_SOURCE="${MONGO_URI_SOURCE##*/}"
 DB_NAME_TARGET="${MONGO_URI_TARGET##*/}"
 
-mongodump --uri="${MONGO_URI_SOURCE}" --forceTableScan --archive | mongorestore --uri="${MONGO_URI_TARGET}" --archive --nsInclude="${DB_NAME_SOURCE}.*" --nsFrom="${DB_NAME_SOURCE}.*" --nsTo="${DB_NAME_TARGET}.*" --drop
+exclude_collection=""
+
+if [ -n "$EXCLUDE_COLLECTION" ]
+  then
+    for collection in $EXCLUDE_COLLECTION; do
+      exclude_collection+=" --excludeCollection=$collection"
+    done
+fi
+
+echo "mongodump --uri="${MONGO_URI_SOURCE}" --forceTableScan --archive $exclude_collection | mongorestore --uri="${MONGO_URI_TARGET}" --archive --nsInclude="${DB_NAME_SOURCE}.*" --nsFrom="${DB_NAME_SOURCE}.*" --nsTo="${DB_NAME_TARGET}.*" --drop"
